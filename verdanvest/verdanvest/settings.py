d1c0d60aa9fe54a251c36454ffc987573687d00d
Bianka <biankasarayj@outlook.com>
2023-11-17 14:43:34 -0600

    Agregar interfaz principal

--- verdanvest/verdanvest/settings.py
+++ verdanvest/verdanvest/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'home.apps.HomeConfig',
     'catalogo.apps.CatalogoConfig',
     'compras.apps.ComprasConfig',
     'dashboard_marca.apps.DashboardMarcaConfig',

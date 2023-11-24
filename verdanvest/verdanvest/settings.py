10021d7b1d6e127d5fff05619ff58687e5085a70
Jaramillo <aj8984524@gmail.com>
2023-11-24 15:44:41 -0600

    Actualizacion interfaces

--- verdanvest/verdanvest/settings.py
+++ verdanvest/verdanvest/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
             'ENGINE': 'django.db.backends.mysql',
             'NAME': 'Verdanvest',
             'USER': 'root',
-            'PASSWORD': 'Pedro061203',
+            'PASSWORD': 'JJLovesU1!',
             'HOST': 'localhost',
             'PORT': '3306',
         }

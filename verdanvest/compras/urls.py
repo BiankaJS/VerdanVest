e6d685e2471fe9b1307ec25b0c970cf6d74afe80
Pedro Flores <pedro.r.flores13@gmail.com>
2023-11-24 16:11:14 -0600

    Actualizacion de urls

--- verdanvest/compras/urls.py
+++ verdanvest/compras/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  
+    path('shipment/', views.shipment_view, name='shipment'),
+    path('shoppingcart/', views.shoppingcart_view, name='shoppingcart')
 ]
--- verdanvest/compras/views.py
+++ verdanvest/compras/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def shipment_view(request):
+    return render(request, 'compras/shipment.html')
+
+def shoppingcart_view(request):
+    return render(request, 'compras/shoppingcart.html')

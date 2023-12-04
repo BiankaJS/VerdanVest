c47d5221fd74a8647d5ece8951e46f3ca1b0fa0f
Bianka <bianka.juarez@novacore.mx>
2023-12-04 17:13:20 -0600

    Agregar producto a carrito

--- verdanvest/catalogo/urls.py
+++ verdanvest/catalogo/urls.py
@@ -5,5 +5,6 @@ app_name='catalog'
 urlpatterns = [
   path('', views.catalog, name='Catalog'),
   path('<int:categoryId>', views.catalog, name='Catalog_Id'),
-  path('detail/<int:productId>', views.productDetail, name = 'ProductDetail')
+  path('detail/<int:productId>', views.productDetail, name ='ProductDetail'),
+  path('add/<int:productId>', views.addProductCart, name='addProductCart')
 ]
--- verdanvest/catalogo/views.py
+++ verdanvest/catalogo/views.py
@@ -1,20 +1,25 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from catalogo.models import Producto, Categoria, Marca
 from ingredientes_materiales.models import Material, Ingrediente
+from compras.models import CarritoCompraDetalle, CarritoCompra
 
 def productDetail(request, productId):
-  try:
-    product = get_object_or_404(Producto, pk=productId)
-    print(product)
-    return render(request, "catalogo/detailproduct.html", { 'product': product })
-  except:
-    return redirect("catalogo/catalog.html", {"error_message": "Ocurred an error"})
-  #return render(request, "catalogo/detailproduct.html")
+    try:
+        product = get_object_or_404(Producto, pk=productId)
+        print(product.nombre)
+        return render(request, "catalogo/detailproduct.html", { 'product': product })
+    except Producto.DoesNotExist:
+        return redirect("catalogo/catalog.html")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return redirect("catalogo/catalog.html", {"error_message": "Ocurred an error"})
 
 def catalog(request, categoryId=None):
   try: 
     if categoryId is not None:
       products = Producto.objects.filter(categoria__id=categoryId)
+      categoria = Categoria.objects.get(pk=categoryId)
+
     else:
       products = Producto.objects.all()
 
@@ -24,6 +29,7 @@ def catalog(request, categoryId=None):
     ingredients = Ingrediente.objects.all() if Ingrediente is not None else []
 
     context = {
+      'categoria': categoria.nombre,
       'products': products,
       'categories': categories,
       'brandies': brandies,
@@ -47,3 +53,17 @@ def catalog(request, categoryId=None):
       'ingredients': ingredients
     }
     return render(request, 'catalogo/catalog.html', context)
+
+def addProductCart(request, productId):
+  if(request.user):
+    try: 
+      cart = CarritoCompra.objects.get(usuario_id=request.user.id)
+    except: 
+      cart = CarritoCompra.objects.create(usuario=request.user, subtotal=0, total=0)
+    if(request.method == "POST"):
+      quantity = request.POST.get('quantity')
+      carritoDetalle = CarritoCompraDetalle.objects.create(producto_id=productId, cantidad= quantity, carrito_compra=cart)
+      carritoDetalle.save()
+      return redirect('shopping:cart')
+  else:
+    return redirect('auth:login')

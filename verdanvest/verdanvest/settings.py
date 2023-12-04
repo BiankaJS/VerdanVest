ffa44604da4834b6c16c27b069a93c3853f2ebaa
Pedro Flores <pedro.r.flores13@gmail.com>
2023-12-03 19:19:35 -0600

    Ya guarda usuario X2

--- verdanvest/verdanvest/settings.py
+++ verdanvest/verdanvest/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
             'ENGINE': 'django.db.backends.mysql',
             'NAME': 'Verdanvest',
             'USER': 'root',
-            'PASSWORD': 'JJLovesU1!',
+            'PASSWORD': 'Pedro061203',
             'HOST': 'localhost',
             'PORT': '3306',
         }
